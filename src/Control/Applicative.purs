43c9dc7d4cac04cc944773e58b3961aa974be715
Phil Freeman <paf31@cantab.net>
2014-04-03 17:10:04 -0700

    Fixes for no overlapping instances

--- src/Control/Applicative.purs
+++ src/Control/Applicative.purs
@@ -5,14 +5,14 @@ import Prelude
 infixl 4 <*
 infixl 4 *>
 
-(<*) :: forall a b f. (Applicative f) => f a -> f b -> f a
+(<*) :: forall a b f. (Functor f, Applicative f) => f a -> f b -> f a
 (<*) x y = const <$> x <*> y
 
-(*>) :: forall a b f. (Applicative f) => f a -> f b -> f b
+(*>) :: forall a b f. (Functor f, Applicative f) => f a -> f b -> f b
 (*>) x y = const id <$> x <*> y
 
-lift2 :: forall a b c f. (Applicative f) => (a -> b -> c) -> f a -> f b -> f c
+lift2 :: forall a b c f. (Functor f, Applicative f) => (a -> b -> c) -> f a -> f b -> f c
 lift2 f x y = f <$> x <*> y
 
-lift3 :: forall a b c d f. (Applicative f) => (a -> b -> c -> d) -> f a -> f b -> f c -> f d
+lift3 :: forall a b c d f. (Functor f, Applicative f) => (a -> b -> c -> d) -> f a -> f b -> f c -> f d
 lift3 f x y z = f <$> x <*> y <*> z
