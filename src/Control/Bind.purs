f3e3c386687fe25dd07b6c7049331a7316654d75
Gary Burgess <gary.burgess@gmail.com>
2014-05-19 12:53:12 +0100

    Add (>>)

--- src/Control/Bind.purs
+++ src/Control/Bind.purs
@@ -1,20 +1,23 @@
 module Control.Bind where
 
-  infixr 1 =<<
-  infixr 1 >=>
-  infixr 1 <=<
+infixr 1 =<<
+infixr 1 >=>
+infixr 1 <=<
 
-  (=<<) :: forall a b m. (Bind m) => (a -> m b) -> m a -> m b
-  (=<<) f m = m >>= f
+(>>) :: forall a b m. (Bind m) => m a -> m b -> m b
+(>>) m n = m >>= \_ -> n
 
-  (>=>) :: forall a b c m. (Bind m) => (a -> m b) -> (b -> m c) -> a -> m c
-  (>=>) f g a = f a >>= g
+(=<<) :: forall a b m. (Bind m) => (a -> m b) -> m a -> m b
+(=<<) f m = m >>= f
 
-  (<=<) :: forall a b c m. (Bind m) => (b -> m c) -> (a -> m b) -> a -> m c
-  (<=<) f g a = f =<< g a
+(>=>) :: forall a b c m. (Bind m) => (a -> m b) -> (b -> m c) -> a -> m c
+(>=>) f g a = f a >>= g
 
-  join :: forall a m. (Bind m) => m (m a) -> m a
-  join m = m >>= id
+(<=<) :: forall a b c m. (Bind m) => (b -> m c) -> (a -> m b) -> a -> m c
+(<=<) f g a = f =<< g a
 
-  ifM :: forall a m. (Bind m) => m Boolean -> m a -> m a -> m a
-  ifM cond t f = cond >>= \cond' -> if cond' then t else f
+join :: forall a m. (Bind m) => m (m a) -> m a
+join m = m >>= id
+
+ifM :: forall a m. (Bind m) => m Boolean -> m a -> m a -> m a
+ifM cond t f = cond >>= \cond' -> if cond' then t else f
