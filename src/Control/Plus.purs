965b24951bb8c62ec2dd01225d3b39ea94c27f60
Harry Garrood <harry@garrood.me>
2015-03-04 14:25:06 +0000

    consistency: 'type class' with a space

--- src/Control/Plus.purs
+++ src/Control/Plus.purs
@@ -2,7 +2,7 @@ module Control.Plus where
 
 import Control.Alt
 
--- | The `Plus` type class extends the `Alt` typeclass with a value that
+-- | The `Plus` type class extends the `Alt` type class with a value that
 -- | should be the left and right identity for `(<|>)`.
 -- |
 -- | `Plus` instances should satisfy the following laws:
