f14399dcd657739745de74cc2cf4ebc3595aadee
Gary Burgess <gary.burgess@gmail.com>
2016-09-30 23:08:33 +0100

    Remove reference to `return` from Comonad doc

--- src/Control/Comonad.purs
+++ src/Control/Comonad.purs
@@ -11,8 +11,7 @@ import Data.Functor (class Functor, map, void, ($>), (<#>), (<$), (<$>))
 -- | `Comonad` extends the `Extend` class with the `extract` function
 -- | which extracts a value, discarding the comonadic context.
 -- |
--- | `Comonad` is the dual of `Monad`, and `extract` is the dual of
--- | `pure` or `return`.
+-- | `Comonad` is the dual of `Monad`, and `extract` is the dual of `pure`.
 -- |
 -- | Laws:
 -- |
