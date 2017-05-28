c393ee50cfb6dcbcef9b8bcd12b681cb9e3cdd9f
Matthew Leon <ml@matthewleon.com>
2017-05-28 02:43:38 +0100

    trivial Lazy instance for functions (#38)
    * trivial Lazy instance for functions
    
    * make lazyFn defer actually defer fn call
    
    Add tests for this.
    
    * relax types in tests

--- src/Control/Lazy.purs
+++ src/Control/Lazy.purs
@@ -1,6 +1,6 @@
 module Control.Lazy where
 
-import Data.Unit (Unit)
+import Data.Unit (Unit, unit)
 
 -- | The `Lazy` class represents types which allow evaluation of values
 -- | to be _deferred_.
@@ -9,6 +9,9 @@ import Data.Unit (Unit)
 -- | be used to delay evaluation.
 class Lazy l where
   defer :: (Unit -> l) -> l
+
+instance lazyFn :: Lazy (a -> b) where
+  defer f = \x -> f unit x
 
 -- | `fix` defines a value as the fixed point of a function.
 -- |
