57a3758daf38e1ccc57b6cea335d4288be9cd6b1
milesfrain <milesfrain@users.noreply.github.com>
2021-01-09 16:34:37 -0800

    Minor simplification of foreign arrayExtend
    Seems like taking the optional array argument in the map callback is unnecessary (unless there's some performance reason for writing it this way).
    I think this simplified version will make it more obvious what's going on.

--- src/Control/Extend.js
+++ src/Control/Extend.js
@@ -2,7 +2,7 @@
 
 exports.arrayExtend = function(f) {
   return function(xs) {
-    return xs.map(function (_, i, xs) {
+    return xs.map(function (_, i) {
       return f(xs.slice(i));
     });
   };
