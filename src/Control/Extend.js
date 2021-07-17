89cdd0284bd05655a42c5a1f6ad79ab92858cad2
Cyril Sobierajewicz <sobierajewicz.cyril@gmail.com>
2021-07-17 20:05:45 +0200

    Convert foreign modules to try bundling with esbuild

--- src/Control/Extend.js
+++ src/Control/Extend.js
@@ -1,6 +1,6 @@
 "use strict";
 
-exports.arrayExtend = function(f) {
+export var arrayExtend = function(f) {
   return function(xs) {
     return xs.map(function (_, i, xs) {
       return f(xs.slice(i));
