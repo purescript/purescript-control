0689a4513b7b6bc5a5f62adfa500e9e9b94c42dd
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-11 14:14:03 -0800

    Replaced 'export var' with 'export const'

--- src/Control/Extend.js
+++ src/Control/Extend.js
@@ -1,6 +1,6 @@
 "use strict";
 
-export var arrayExtend = function(f) {
+export const arrayExtend = function(f) {
   return function(xs) {
     return xs.map(function (_, i, xs) {
       return f(xs.slice(i));
