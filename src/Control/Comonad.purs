765752a67420364aa70ee416abf5ae0d881651f7
Samuel Pocino <43758828+spocino@users.noreply.github.com>
2024-02-05 18:10:46 -0500

    Add Function instance to Comonad to match Extend instance

--- src/Control/Comonad.purs
+++ src/Control/Comonad.purs
@@ -7,6 +7,7 @@ module Control.Comonad
 import Control.Extend (class Extend, duplicate, extend, (<<=), (=<=), (=>=), (=>>))
 
 import Data.Functor (class Functor, map, void, ($>), (<#>), (<$), (<$>))
+import Data.Monoid (class Monoid, mempty)
 
 -- | `Comonad` extends the `Extend` class with the `extract` function
 -- | which extracts a value, discarding the comonadic context.
@@ -19,3 +20,6 @@ import Data.Functor (class Functor, map, void, ($>), (<#>), (<$), (<$>))
 -- | - Right Identity: `extract (f <<= xs) = f xs`
 class Extend w <= Comonad w where
   extract :: forall a. w a -> a
+
+instance comonadFn :: Monoid m => Comonad ((->) m) where
+  extract f = f mempty
